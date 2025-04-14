6f88ddae1845b62c0a739cbaa85e149f569c2c40
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-04-14 19:01:41 +0300

    11.9

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+// cypress.config.js
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5000',
+    supportFile: false
+  }
+})
+
