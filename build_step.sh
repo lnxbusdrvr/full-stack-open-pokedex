ceab8849f56d8631068c6acaf04f05a94d357b42
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-04-15 12:27:01 +0300

    Add npm start to bg

--- build_step.sh
+++ build_step.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -e # Stops script if failure
+
 echo "build"
+npm install
+npm run eslint
+npm run test
+npm run start & SERVER_PID=$! # put server ti background
+npm run test:e2e
+kill $SERVER_PID # kill background server
 npm run build
